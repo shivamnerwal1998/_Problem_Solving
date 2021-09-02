b2d3b4df2be93551652677f8c67ea93c9aa5a494
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-09-03 00:42:18 +0530

    Create Tree_Level_Order_Traveresal.cpp

---
+++ Tree_Level_Order_Traveresal.cpp
@@ -0,0 +1,16 @@
+
+    void levelOrder(Node * root) {
+        
+        queue<Node*> q ; 
+        q.push(root) ;
+        while( !q.empty() )
+        {
+            Node *temp = q.front() ; 
+            cout<< temp->data<<" " ;
+            q.pop() ;
+            if( temp->left != NULL ) q.push(temp->left) ; 
+            if( temp->right != NULL ) q.push( temp->right ) ;              
+        }
+        
+    
+    }
